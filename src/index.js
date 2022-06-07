afed82cdc7a75713863ad84005e10cfff84ecf2b
fenix1851 <foxyess@mail.ru>
2022-06-07 05:36:04 +0300

    ended working with regexp, now programm can define neiumbers with different signes

--- src/index.js
+++ src/index.js
@@ -7,27 +7,35 @@ const {Telegraf} = require("telegraf")
 const init = async (bot) =>{
     bot.start(ctx=>{console.log('1')})
     bot.on('message',(ctx)=>{
-        if(ctx.message.text[0].match(/[0-9|+|-]/)){
-            let text = ctx.message.text
-            text = text.replace(/\s/g, '');
-            let number = text.match(/[1-9|+|-][0-9]{0,}/);
-            ctx.reply(number.toString())
-            console.log(number[0][0])
-            if (+number[0][0].match[/[1-9|+]/g]) {
+      if(ctx.message.text){
+        let text = ctx.message.text;
+
+        //console.log(text.split(/\n/));
+        textArr = text.split(/\n/)
+        for (let text of textArr) {
+          if (text[0].match(/[0-9|+|-]/g) !== null) {
+            text = text.replace(/\s/g, "");
+            let number = text.match(/[1-9|+|-][0-9]{0,}/g)[0].toString();
+            //ctx.reply(number.match(/[0-9|+]/g));
+            //console.log(number.match(/[0-9|+]/).toString());
+            if (number[0] == number.match(/[0-9|+]/)) {
               console.log("Plus");
+              if (number[0] == "+") number = number.slice(1);
+              console.log(number);
             } else {
               console.log("Minus");
+              console.log(number);
             }
-
+          }   
         }
-
+    }
     })
     return bot
 }
 
 init(new Telegraf(BOT_TOKEN, { polling: true })).then(async (bot) => {
   await bot.launch();
-  console.log(`Launched ${new Date()}`);
+  console.log(`Launch ${new Date()}`);
 });
 
 module.exports = init;
